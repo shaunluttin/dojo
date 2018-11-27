5a7c190852258f2bf668cddf8992259294d3a3c4
Shaun Luttin <shaunluttin@gmail.com>
2018-11-27 08:54:18 -0800

    'Game' does not contain a definition for 'roll'

--- BowlingGame05/BowlingGameTest/BowlingGameTest.cs
+++ BowlingGame05/BowlingGameTest/BowlingGameTest.cs
@@ -10,6 +10,8 @@ namespace BowlingGameTest
         public void testGutterGame()
         {
             var g = new Game(); 
+            for (var i = 0; i < 20; ++i)
+                g.roll(0);
         }
     }
 }
