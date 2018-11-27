27b02e21a02b1aac43683a705246c647999a4311
Shaun Luttin <shaunluttin@gmail.com>
2018-11-27 09:06:45 -0800

    Factor out shared test setup

--- BowlingGame05/BowlingGameTest/BowlingGameTest.cs
+++ BowlingGame05/BowlingGameTest/BowlingGameTest.cs
@@ -6,24 +6,31 @@ namespace BowlingGameTest
 {
     public class BowlingGameTest
     {
+        private Game _g;
+
+        private void setup()
+        {
+            _g = new Game(); 
+        }
+
         [Fact]
         public void testGutterGame()
         {
-            var g = new Game(); 
+            setup();
             for (var i = 0; i < 20; ++i)
-                g.roll(0);
+                _g.roll(0);
 
-            Assert.Equal(0, g.score());
+            Assert.Equal(0, _g.score());
         }
 
         [Fact]
         public void testAllOnes()
         {
-            var g = new Game(); 
+            setup();
             for (var i = 0; i < 20; ++i)
-                g.roll(1);
+                _g.roll(1);
 
-            Assert.Equal(20, g.score());
+            Assert.Equal(20, _g.score());
         }
     }
 }
