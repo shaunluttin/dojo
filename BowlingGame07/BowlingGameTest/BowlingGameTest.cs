8537f60a237174a244e5f26f205ad86ff6440ef0
Shaun Luttin <shaunluttin@gmail.com>
2018-11-28 08:53:08 -0800

    'Game' does not contain a definition for 'score'

--- BowlingGame07/BowlingGameTest/BowlingGameTest.cs
+++ BowlingGame07/BowlingGameTest/BowlingGameTest.cs
@@ -12,6 +12,8 @@ namespace BowlingGameTest
             var g = new Game();
             for(var i = 0; i < 20; ++i) 
                 g.roll(0);
+
+            Assert.Equal(0, g.score());
         }
     }
 }
