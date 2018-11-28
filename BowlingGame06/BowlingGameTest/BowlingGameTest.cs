a7ea65e256200d2ac7b6b23e3f0980d6eda96dcf
Shaun Luttin <shaunluttin@gmail.com>
2018-11-27 20:09:49 -0800

    Introduce BowlingGame.Game class

--- BowlingGame06/BowlingGameTest/BowlingGameTest.cs
+++ BowlingGame06/BowlingGameTest/BowlingGameTest.cs
@@ -1,10 +1,12 @@
 using System;
 using Xunit;
+using BowlingGame;
 
 namespace BowlingGameTest
 {
     public class BowlingGameTest
     {
+        [Fact]
         public void testGutter()
         {
             var g = new Game();
