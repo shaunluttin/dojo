27975250924684ab2b7d60e6dc2c6ccee59c82a5
Shaun Luttin <shaunluttin@gmail.com>
2018-11-29 08:49:33 -0800

    Continue redesigning for spare calculation

--- BowlingGame08/BowlingGame/Game.cs
+++ BowlingGame08/BowlingGame/Game.cs
@@ -4,21 +4,22 @@ namespace BowlingGame
 {
     public class Game
     {
-        private int _score;
-
         private int[] _rolls = new int[21];
 
         private int _currentRoll;
 
         public void roll(int pins)
         {
-           _score += pins; 
            _rolls[_currentRoll++] = pins;
         }
 
         public int score()
         {
-            return _score;
+            int score = 0;
+            for (var i = 0; i < _rolls.Length; ++i)
+                score += _rolls[i];
+
+            return score;
         }
     }
 }
