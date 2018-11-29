669bbefdfda30894186483c94b7c9f28b0b45a75
Shaun Luttin <shaunluttin@gmail.com>
2018-11-29 08:47:28 -0800

    Begin redesign to include spares

--- BowlingGame08/BowlingGame/Game.cs
+++ BowlingGame08/BowlingGame/Game.cs
@@ -6,9 +6,14 @@ namespace BowlingGame
     {
         private int _score;
 
+        private int[] _rolls = new int[21];
+
+        private int _currentRoll;
+
         public void roll(int pins)
         {
            _score += pins; 
+           _rolls[_currentRoll++] = pins;
         }
 
         public int score()
