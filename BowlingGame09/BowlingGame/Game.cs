056c0392f4837b7402cd7f37365e9eb5a54c9ea5
Shaun Luttin <shaunluttin@gmail.com>
2018-11-30 15:12:57 -0800

    Delete unused _score field

--- BowlingGame09/BowlingGame/Game.cs
+++ BowlingGame09/BowlingGame/Game.cs
@@ -4,14 +4,12 @@ namespace BowlingGame
 {
     public class Game
     {
-        private int _score;
         private int[] _rolls = new int[21];
         private int _currentRoll;
 
         public void roll(int pins) 
         {
             _rolls[_currentRoll++] = pins;
-            _score += pins;
         }
 
         public int score() 
