c7369c5663e313f1a450c77bdce8d8a338239c99
Shaun Luttin <shaunluttin@gmail.com>
2018-12-01 09:44:08 -0800

    16. Refactor roll/score in preparation for strikes

--- BowlingGame10/BowlingGame/Game.cs
+++ BowlingGame10/BowlingGame/Game.cs
@@ -5,15 +5,23 @@ namespace BowlingGame
     public class Game
     {
         private int _score;
+        private int _currentRoll;
+        private int[] _rolls = new int[21];
 
         public void roll(int pins)
         {
             _score += pins;
+
+            _rolls[_currentRoll++] = pins;
         }
 
         public int score()
         {
-            return _score;    
+            var score = 0;
+            for (var i = 0; i < _rolls.Length; ++i)
+                score += _rolls[i];
+
+            return score;
         }
     }
 }
