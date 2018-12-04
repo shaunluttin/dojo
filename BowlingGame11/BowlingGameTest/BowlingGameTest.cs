50107b027eb9d0a0a376b04c45dd100a483e632b
Shaun Luttin <shaunluttin@gmail.com>
2018-12-03 20:37:26 -0800

    15. Factor out shared Game instantiation

--- BowlingGame11/BowlingGameTest/BowlingGameTest.cs
+++ BowlingGame11/BowlingGameTest/BowlingGameTest.cs
@@ -6,24 +6,29 @@ namespace BowlingGameTest
 {
     public class BowlingGameTest
     {
+        private Game _g;
+
+        public BowlingGameTest()
+        {
+            _g = new Game();
+        }
+
         [Fact]
         public void testGutterGame()
         {
-            var g = new Game();
             for (var i = 0; i  < 20; i++) // ugly magic number 20
-                g.roll(0);
+                _g.roll(0);
 
-            Assert.Equal(0, g.score());
+            Assert.Equal(0, _g.score());
         }
 
         [Fact]
         public void testAllOnes()
         {
-            var g = new Game();
             for (var i = 0; i  < 20; i++) // ugly magic number 20
-                g.roll(1);
+                _g.roll(1);
 
-            Assert.Equal(20, g.score());
+            Assert.Equal(20, _g.score());
         }
     }
 }
