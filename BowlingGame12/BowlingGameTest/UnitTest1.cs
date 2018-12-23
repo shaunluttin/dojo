30b83f38e930dc9af3186fe7578684fbab327759
Shaun Luttin <shaunluttin@gmail.com>
2018-12-23 14:39:41 -0800

    05 Add Game to BowlingGameTest

--- BowlingGame12/BowlingGameTest/UnitTest1.cs
+++ BowlingGame12/BowlingGameTest/UnitTest1.cs
@@ -1,10 +1,12 @@
 using System;
 using Xunit;
+using BowlingGame;
 
 namespace BowlingGameTest
 {
     public class UnitTest1
     {
+        [Fact]
         public void testGutterGame()
         {
             var g = new Game();
