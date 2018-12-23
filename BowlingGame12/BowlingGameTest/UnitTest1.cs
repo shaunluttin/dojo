be97046779d8c7aaae24ff401edf56027ca9e0ed
Shaun Luttin <shaunluttin@gmail.com>
2018-12-23 14:43:15 -0800

    06 'Game' does not contain 'Roll'

--- BowlingGame12/BowlingGameTest/UnitTest1.cs
+++ BowlingGame12/BowlingGameTest/UnitTest1.cs
@@ -10,6 +10,11 @@ namespace BowlingGameTest
         public void testGutterGame()
         {
             var g = new Game();
+
+            for (var i = 0; i < 20; ++i) 
+            {
+                g.Roll(); 
+            }
         }
     }
 }
