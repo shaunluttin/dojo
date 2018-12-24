32686bd34d751bb6b000fae3cd0f042686be5292
Shaun Luttin <shaunluttin@gmail.com>
2018-12-23 19:17:44 -0800

    13 Refactor in preparation for rollOneSpare

--- BowlingGame12/BowlingGame/Game.cs
+++ BowlingGame12/BowlingGame/Game.cs
@@ -4,16 +4,24 @@ namespace BowlingGame
 {
     public class Game
     {
-        private int _score;
+        private int _roll = 0;
+
+        private int[] _rolls = new int[20]; // magic number
 
         public void Roll(int pins)
         {
-            this._score += pins;
+            this._rolls[this._roll++] = pins;
         }
 
         public int Score()
         {
-            return this._score;
+            var score = 0;
+            for (var i = 0; i < 20; i++) 
+            {
+                 score += this._rolls[i];
+            }
+
+            return score;
         }
     }
 }
--- BowlingGame12/BowlingGameTest/BowlingGameTest.cs
+++ BowlingGame12/BowlingGameTest/BowlingGameTest.cs
@@ -29,20 +29,20 @@ namespace BowlingGameTest
             Assert.Equal(20, this._g.Score());
         }
 
-        [Fact]
-        public void TestOneSpare()
-        {
-            // spare, 10
-            this._g.Roll(5);
-            this._g.Roll(5);
-
-            // after spare, 3 plus bonus of 3
-            this._g.Roll(3);
-
-            RollMany(17, 0);
-
-            Assert.Equal(16, this._g.Score());
-        }
+//        [Fact]
+//        public void TestOneSpare()
+//        {
+//            // spare, 10
+//            this._g.Roll(5);
+//            this._g.Roll(5);
+//
+//            // after spare, 3 plus bonus of 3
+//            this._g.Roll(3);
+//
+//            RollMany(17, 0);
+//
+//            Assert.Equal(16, this._g.Score());
+//        }
 
         private void RollMany(int rolls, int pins)
         {
