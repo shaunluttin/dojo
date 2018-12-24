c1174984464885fd1f17b4ef2d3ac00f53198a92
Shaun Luttin <shaunluttin@gmail.com>
2018-12-23 18:53:16 -0800

    09 Pass testAllOnes (2/2)

--- BowlingGame12/BowlingGame/Game.cs
+++ BowlingGame12/BowlingGame/Game.cs
@@ -4,14 +4,16 @@ namespace BowlingGame
 {
     public class Game
     {
+        private int _score;
+
         public void Roll(int pins)
         {
-        
+            this._score += pins;
         }
 
         public int Score()
         {
-            return 0;
+            return this._score;
         }
     }
 }
