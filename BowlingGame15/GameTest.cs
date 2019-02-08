4892f6f3d302be5ee67cffdf070a753b8f43594b
Shaun Luttin <shaunluttin@gmail.com>
2019-02-08 12:24:29 -0800

    04 Fail testAllOnes: Expected 20 Actual 0

--- BowlingGame15/GameTest.cs
+++ BowlingGame15/GameTest.cs
@@ -16,5 +16,18 @@ namespace BowlingGame15
 
             Assert.Equal(0, game.Score());
         }
+
+        [Fact]
+        // ugly repetition
+        public void testAllOnes()
+        {
+            var game = new Game();
+
+            for (var i = 0; i < 20; i++) {
+                game.Roll(1);
+            }
+
+            Assert.Equal(20, game.Score());
+        }
     }
 }
